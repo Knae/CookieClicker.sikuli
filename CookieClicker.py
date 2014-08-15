1260c1b7adf73d905c1eade718a30d4347f49a10
Chuang Sing, Kee <general124@hotmail.com>
2014-08-15 19:49:55 +1200

    Initial Creation
    Initial upload of auto-clicker

---
+++ CookieClicker.py
@@ -0,0 +1,5 @@
+
+while exists ( Pattern("1381753192390.png").similar(0.60) ): 
+    click( Pattern("1381753192390.png").similar(0.62) )
+    if exists ( Pattern("GoldCookie.png").similar(0.60) ):
+        click (Pattern("GoldCookie.png").similar(0.60) )
